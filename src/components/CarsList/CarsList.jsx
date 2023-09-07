fe177cc1383d0efb6e038aade5bb651f25e05c3a
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-09-07 18:55:44 +0300

    fix bug

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -1,14 +1,13 @@
 import { List } from './CarsList.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectCars, selectIsLoading } from 'redux/selectors';
+import { selectCars } from 'redux/selectors';
 import { useEffect } from 'react';
 import { fetchCars } from 'redux/operations';
 import CarItem from 'components/CarItem/CarItem';
-// import { Loading } from 'notiflix';
 
 export const CarsList = () => {
   const cars = useSelector(selectCars);
-  // const isLoading = useSelector(selectIsLoading);
+
   const dispatch = useDispatch();
 
   console.log(cars);
@@ -18,13 +17,10 @@ export const CarsList = () => {
   }, [dispatch]);
 
   return (
-    <>
-      {/* {isLoading ? Loading.arrows() : Loading.remove()} */}
-      <List>
-        {cars.map(car => (
-          <CarItem key={car.id} item={car} />
-        ))}
-      </List>
-    </>
+    <List>
+      {cars.map(car => (
+        <CarItem key={car.id} item={car} />
+      ))}
+    </List>
   );
 };
